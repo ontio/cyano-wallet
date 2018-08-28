ef5458ac049d62b9144b69e5a83ec0b09b0b9755
Matus Zamborsky <zamborsky@gmail.com>
2018-08-28 14:20:28 +0200

    bumped dapi
    Signed-off-by: Matus Zamborsky <zamborsky@gmail.com>

--- src/background/dapp/index.ts
+++ src/background/dapp/index.ts
@@ -1,4 +1,4 @@
-import { provider } from 'ontology-dapi';
+import { client, provider } from 'ontology-dapi';
 import { assetApi as asset } from './asset';
 import { identityApi as identity } from './identity';
 import { messageApi as message } from './message';
@@ -16,6 +16,7 @@ export function initDApiProvider() {
       network,
       provider: providerApi,
       smartContract,
+      utils: client.api.utils
     },
   });
 }
