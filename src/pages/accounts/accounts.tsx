4c6ef5129e3057d6880ee28ca481f7c18e4c53d4
Vladimir Stavitskiy <vladimir.stavitskiy@apriorit.com>
2019-03-27 18:01:15 +0200

    add refresh balance on account change

--- src/pages/accounts/accounts.tsx
+++ src/pages/accounts/accounts.tsx
@@ -1,29 +1,13 @@
-/*
- * Copyright (C) 2018 Matus Zamborsky
- * This file is part of The Ontology Wallet&ID.
- *
- * The The Ontology Wallet&ID is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The Ontology Wallet&ID is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with The Ontology Wallet&ID.  If not, see <http://www.gnu.org/licenses/>.
- */
 import * as React from "react";
 import { RouterProps } from "react-router";
 import { bindActionCreators, Dispatch } from "redux";
 import { encodeWallet, getWallet } from "src/api/authApi";
 import { reduxConnect, withProps } from "../../compose";
-import { GlobalState } from "../../redux";
+import { GlobalState, getStore } from "../../redux";
 import Actions from "../../redux/actions";
 import { finishLoading, startLoading } from "../../redux/loader/loaderActions";
 import { AccountsView, Props } from "./accountsView";
+import { refreshBalance } from "../../balanceProvider";
 
 const mapStateToProps = (state: GlobalState) => ({
   loading: state.loader.loading,
@@ -45,7 +29,7 @@ const enhancer = (Component: React.ComponentType<Props>) => (props: RouterProps)
   reduxConnect(mapStateToProps, mapDispatchToProps, (reduxProps, actions) => {
     const wallet = getWallet(reduxProps.wallet!);
     const accounts = wallet.accounts.map(account => account.address.toBase58());
-
+    const store = getStore();
     return withProps(
       {
         accounts,
@@ -56,7 +40,7 @@ const enhancer = (Component: React.ComponentType<Props>) => (props: RouterProps)
           await actions.accountsStartLoading();
           await actions.accountsSetWallet(encodedWallet);
 
-          // await getBackgroundManager().refreshBalance();
+          await refreshBalance(store);
 
           await actions.accountsFinishLoading();
 
