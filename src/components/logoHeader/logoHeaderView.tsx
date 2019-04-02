48cb10cd9db7c718f16d4b885bda5a625da8eb80
Vladimir Stavitskiy <vladimir.stavitskiy@apriorit.com>
2019-04-02 11:06:21 +0300

    fix adaptive

--- src/components/logoHeader/logoHeaderView.tsx
+++ src/components/logoHeader/logoHeaderView.tsx
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2018 Matus Zamborsky
- * This file is part of The Ontology Wallet&ID.
- *
- * The The Ontology Wallet&ID is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The Ontology Wallet&ID is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with The Ontology Wallet&ID.  If not, see <http://www.gnu.org/licenses/>.
- */
 import * as React from "react";
 import { Button } from "semantic-ui-react";
 import { View } from "../view";
@@ -28,35 +11,40 @@ export interface Props {
 }
 
 export const LogoHeaderView: React.SFC<Props> = props => (
-  <View className="logoHeader space-betweem">
-    <View>
-      <img height="30" src={require("../../assets/logo-main.png")} />
-      <span>Onyx Wallet</span>
+  <header>
+    <View className="logoHeader space-betweem">
+      <View>
+        <img height="30" src={require("../../assets/logo-main.png")} />
+        <span>Onyx Wallet</span>
+      </View>
+
+      <View orientation="row" className="buttons">
+        {props.showAccounts ? (
+          <Button
+            onClick={props.handleAccounts}
+            size="big"
+            compact={true}
+            basic={true}
+            icon="exchange"
+            data-tooltip="Change account"
+            data-position="bottom right"
+          />
+        ) : null}
+        {props.showSettings ? (
+          <Button
+            onClick={props.handleSettings}
+            size="big"
+            compact={true}
+            basic={true}
+            icon="cog"
+            data-tooltip="Settings"
+            data-position="bottom right"
+          />
+        ) : null}
+      </View>
     </View>
-    <View>
+    <View className="justify-center">
       <h1>{props.title}</h1>
     </View>
-    <View orientation="row" className="buttons">
-      { props.showAccounts ? 
-      <Button 
-        onClick={props.handleAccounts} 
-        size="big" 
-        compact={true}
-        basic={true} 
-        icon="exchange" 
-        data-tooltip="Change account" 
-        data-position="bottom right"/> 
-       : (null) }
-      { props.showSettings ? 
-      <Button 
-        onClick={props.handleSettings} 
-        size="big" 
-        compact={true} 
-        basic={true} 
-        icon="cog" 
-        data-tooltip="Settings" 
-        data-position="bottom right"/> 
-      : (null) }
-    </View>
-  </View>
+  </header>
 );
