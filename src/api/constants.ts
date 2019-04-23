173238430f8c7a82a7aa7372a5ee2bfdaa8714c0
Vladimir Stavitskiy <vladimir.stavitskiy@apriorit.com>
2019-04-23 20:44:34 +0300

    add compensator calls

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,14 +1,14 @@
+// export const testOptions = {
+//   text: "TEST",
+//   value: "35.178.63.10"
+// };
+
+// export const prodOptions = {
+//   text: "MAIN",
+//   value: "35.180.188.239"
+// };
+
 export const testOptions = {
-  text: "TEST",
-  value: "35.178.63.10"
-};
-
-export const prodOptions = {
-  text: "MAIN",
-  value: "35.180.188.239"
-};
-
-/* export const testOptions = {
   text: "TEST",
   value: "cepheus5.onyxpay.co"
 };
@@ -16,12 +16,12 @@ export const prodOptions = {
 export const prodOptions = {
   text: "MAIN",
   value: "andromeda1.onyxpay.co"
-}; */
+};
 
 export const headAddress = {
-  test: "b93ade3f201bdc5ab0f9a07283ba4078ff00563e",
+  test: "3a3ac01fda7736d8f342f2e383227ab7aa9a87e4",
   main: "?????"
 };
 
-export const gasCompensatorEndpoint = "http://3.120.190.178:5000";
+export const gasCompensatorEndpoint = "http://3.120.190.178:5001";
 export const restEndpoint = "http://34.244.79.17:600/api/v1";
