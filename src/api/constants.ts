4195397abbef2a6450fc7db1106fa072c5296fb5
ak-toktor <obr@bk.ru>
2019-04-23 15:55:22 +0300

    I changed ip on domain

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,13 +1,13 @@
 export const testOptions: Array<{ text: string; value: string }> = [
   {
     text: "OnyxPay TestNet",
-    value: "35.178.63.10"
+    value: "cepheus5.onyxpay.co"
   }
 ];
 
 export const prodOptions: Array<{ text: string; value: string }> = [
   {
     text: "OnyxPay MainNet",
-    value: "35.180.188.239"
+    value: "andromeda1.onyxpay.co"
   }
 ];
