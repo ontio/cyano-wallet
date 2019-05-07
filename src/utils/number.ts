95ec43a31ca5310de1ab5a569a1a8c55a6a269ce
oleg.sulzhenko <oleg.sulzhenko@apriorit.com>
2019-05-07 18:07:20 +0300

    refactoring after pull request comments

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,23 +1,6 @@
 import BigNumber from "bignumber.js";
 import { AssetType } from "../redux/runtime";
 
-export function convertAmountToBN(amount: number | string | undefined, asset: AssetType, decimals?) {
-  if (amount === undefined) {
-    return new BigNumber(0);
-  }
-
-  let amountBN = new BigNumber(amount);
-
-  if (asset === "OXG") {
-    amountBN = amountBN.div(new BigNumber(Math.pow(10, 9)));
-  } else if (asset === "ONYX") {
-    amountBN = amountBN.div(new BigNumber(Math.pow(10, 8)));
-  } else {
-    amountBN = amountBN.div(new BigNumber(Math.pow(10, decimals)));
-  }
-  return amountBN.toFixed();
-}
-
 export function convertAmountFromStr(amount: string, asset: AssetType) {
   let amountBN = new BigNumber(amount);
   if (asset === "OXG") {
@@ -30,7 +13,21 @@ export function convertAmountFromStr(amount: string, asset: AssetType) {
 }
 
 export function convertAmountToStr(amount: number | string | undefined, asset: AssetType, decimals?) {
-  return convertAmountToBN(amount, asset, decimals).toString();
+  if (amount === undefined) {
+    return new BigNumber(0).toString();
+  }
+
+  let amountBN = new BigNumber(amount);
+
+  if (asset === "OXG") {
+    amountBN = amountBN.div(new BigNumber(Math.pow(10, 9)));
+  } else if (asset === "ONYX") {
+    amountBN = amountBN.div(new BigNumber(Math.pow(10, 8)));
+  } else {
+    amountBN = amountBN.div(new BigNumber(Math.pow(10, decimals)));
+  }
+
+  return amountBN.toFixed().toString();
 }
 
 export function encodeAmount(amount: string, decimals: number) {
