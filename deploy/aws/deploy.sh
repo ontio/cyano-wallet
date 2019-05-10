99897b006c7a46f9eab07493866ac1ef8bc20474
toktor <ak.toktor@gmail.com>
2019-05-10 12:56:17 +0300

    I fixed deployment script

--- deploy/aws/deploy.sh
+++ deploy/aws/deploy.sh
@@ -4,7 +4,6 @@ TAG=$1
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 eval $(aws ecr get-login --region us-east-2 --no-include-email)
-#branch=$TAG docker-compose build
 docker tag onyxchain_wallet:$TAG 866680356172.dkr.ecr.us-east-2.amazonaws.com/onyxchain_wallet:$TAG
 docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/onyxchain_wallet:$TAG
 CLUSTER=OnyxChain-wallet
@@ -14,5 +13,6 @@ then
 	CLUSTER=OnyxChain-wallet-prod
 	SERVICE=OnyxChainWalletProdSrv
 fi
-eval $(aws ecs describe-services --cluster $CLUSTER --region us-east-2 --service $SERVICE)
+
+aws ecs describe-services --cluster $CLUSTER --region us-east-2 --service $SERVICE
 
