e02970cd4af7c1850f0be2b184288208f58f12f4
toktor <ak.toktor@gmail.com>
2019-05-22 19:37:42 +0300

    refactoring

--- deploy/aws/deploy.sh
+++ deploy/aws/deploy.sh
@@ -4,8 +4,7 @@ TAG=$1
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 eval $(aws ecr get-login --region us-east-2 --no-include-email)
-docker tag onyxchain_wallet:$TAG 866680356172.dkr.ecr.us-east-2.amazonaws.com/onyxchain_wallet:$TAG
-docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/onyxchain_wallet:$TAG
+docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/wallet:$TAG
 CLUSTER=OnyxChain-wallet
 SERVICE=WalletService-dev
 if [[ $TAG = "prod" ]]
