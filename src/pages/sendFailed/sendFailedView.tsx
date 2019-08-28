cafc503ffb0a275698e04055f98053b4db4d4cdd
Oleh Hinko <oleh.hinko@apriorit.com>
2019-08-28 18:48:48 +0300

    ONXP-1153 [Wallet] Fix the message when transaction has not completed in time.

--- src/pages/sendFailed/sendFailedView.tsx
+++ src/pages/sendFailed/sendFailedView.tsx
@@ -31,11 +31,11 @@ export const SendFailedView: React.SFC<Props> = (props) => (
     <View orientation="column" className="part gradient">
       <LogoHeader showLogout={true} showAccounts={true} title="Transaction failed" />
       <View content={true} className="spread-around">
-        <View>The transaction timeouted.</View>
+        <View>The transaction timeout.</View>
       </View>
     </View>
     <View orientation="column" fluid={true} content={true} className="spread-around">
-      <Message>Your transaction of {props.amount} {props.asset} to {props.recipient} has not completed in time. This does not mean it necessary failed. Check your balances.</Message>
+      <Message>Your transaction of {props.amount} {props.asset} to {props.recipient} has not completed in time. This does not mean it necessarily failed. Check your balances.</Message>
       <Filler />
       <View className="buttons">
         <Button onClick={props.handleOk}>Ok</Button>
