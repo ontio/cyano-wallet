31ce1223d847f08f68adcfd63ef3e8384be54554
Matus Zamborsky <zamborsky@gmail.com>
2019-10-22 09:11:53 +0200

    small fixes

--- src/popup/backgroundManager.ts
+++ src/popup/backgroundManager.ts
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with The Ontology Wallet&ID.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { History } from 'history';
 import { Rpc } from '@ont-dev/ontology-dapi';
+import { History } from 'history';
 import { browser } from 'webextension-polyfill-ts';
 import { OEP4Token } from '../api/tokenApi';
 
